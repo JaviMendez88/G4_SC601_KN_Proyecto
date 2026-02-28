441d50f224dca7bd5df775bf312036db2a394886
Javier Mendez <88914048+JaviMendez88@users.noreply.github.com>
2026-02-27 21:55:57 -0600

    Week 7. Push Test

--- G4_SC601_KN_Proyecto/Controllers/HomeController.cs
+++ G4_SC601_KN_Proyecto/Controllers/HomeController.cs
@@ -9,16 +9,23 @@ namespace G4_SC601_KN_Proyecto.Controllers
 {
     public class HomeController : Controller
     {
+        #region Index
         public ActionResult Index()
         {
             return View();
         }
 
+
+        #endregion Index
+
+        #region Log In
         public ActionResult Login()
         {
             return View();
         }
 
+
+        #endregion Log In
 
         #region Sign Up
 
