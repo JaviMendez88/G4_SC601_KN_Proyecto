086023e0cca2e80753aaae2f3e1a1dd3f6e901ae
Raid2021 <raul.pdcps@gmail.com>
2026-03-19 23:56:45 -0600

    Acualizado

--- G4_SC601_KN_Proyecto/EntityFramework/producto.cs
+++ G4_SC601_KN_Proyecto/EntityFramework/producto.cs
@@ -26,7 +26,7 @@ namespace G4_SC601_KN_Proyecto.EntityFramework
         public int id_categoria { get; set; }
         public string nombre { get; set; }
         public string descripcion { get; set; }
-        public Nullable<decimal> precio { get; set; }
+        public decimal precio { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<lote> lote { get; set; }
