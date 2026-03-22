18330fca32768aa407bd9de262955a9bf1b7686f
Ange3108 <aguatemala40593@ufide.ac.cr>
2026-03-22 16:56:31 -0600

    Update terumo.sql

--- terumo.sql
+++ terumo.sql
@@ -214,15 +214,29 @@ CREATE TABLE movimiento_inventario (
 
 
 
-CREATE TABLE auditoria (
-    id_auditoria INT IDENTITY(1,1) PRIMARY KEY,
-    id_usuario INT NOT NULL,
-    accion VARCHAR(50),
-    tabla_afectada VARCHAR(50),
-    fecha DATETIME2 DEFAULT SYSDATETIME(),
-    CONSTRAINT fk_auditoria_usuario 
-        FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
-);
+CREATE TABLE [dbo].[auditoria](
+	[id_auditoria] [int] IDENTITY(1,1) NOT NULL,
+	[NombreTabla] [varchar](100) NOT NULL,
+	[accion] [varchar](10) NOT NULL,
+	[OldValues] [varchar](max) NOT NULL,
+	[NewValues] [varchar](max) NOT NULL,
+	[id_usuario] [int] NOT NULL,
+	[date] [datetime] NOT NULL,
+	[NombreUser] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_auditoria] PRIMARY KEY CLUSTERED 
+(
+	[id_auditoria] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[auditoria]  WITH CHECK ADD  CONSTRAINT [FK_usuario_auditado] FOREIGN KEY([id_usuario])
+REFERENCES [dbo].[usuario] ([id_usuario])
+GO
+
+ALTER TABLE [dbo].[auditoria] CHECK CONSTRAINT [FK_usuario_auditado]
+GO
+
 
 
 
