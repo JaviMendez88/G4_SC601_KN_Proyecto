fba8408265d440427434bb6aaaadc47602390bb2
Ange3108 <aguatemala40593@ufide.ac.cr>
2026-04-07 21:08:52 -0600

    validaciones desde el materialModel para editar y crear

--- G4_SC601_KN_Proyecto/Models/materialModel.cs
+++ G4_SC601_KN_Proyecto/Models/materialModel.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,18 +9,30 @@ namespace G4_SC601_KN_Proyecto.Models
 {
     public class materialModel
     {
+        [Required]
         public int idMaterial { get; set; }
+        [Required]
         public int idFamily { get; set; }
+        [Required]
         public int idParent{ get; set; }
+        [Required]
         public string descriptionM { get; set; }
+        [Required]
         public int countOrder { get; set; }
+        [Required]
         public int Qty { get; set; }
+        [Required]
         public int labor { get; set; }
+        [Required]
         public decimal IndOVH { get; set; }
+        [Required]
         public int  Scrap_Allowance { get; set; }
+        [Required]
         public decimal materialPrice { get; set; }
+        [Required]
         public decimal totalVariance { get; set; }
-     
+        
+
 
 
 
