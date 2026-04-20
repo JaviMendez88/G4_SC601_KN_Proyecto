209d9880073756b06fba0718b0dc44a32f1d3b3e
Javier Mendez <88914048+JaviMendez88@users.noreply.github.com>
2026-04-19 18:58:49 -0600

    Add Model
    Modelo test para visualización de j charts

---
+++ G4_SC601_KN_Proyecto/Models/DashboardInventarioViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace G4_SC601_KN_Proyecto.Models
+{
+    public class DashboardInventarioViewModel
+    {
+        public List<StockViewModel> StockActual { get; set; }
+        public List<StockViewModel> ProximosVencimientos { get; set; }
+
+        public int TotalMateriales { get; set; }
+        public int TotalLotes { get; set; }
+        public int TotalStock { get; set; }
+    }
+
+
+}
+}
